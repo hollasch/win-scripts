af930b8a62f99abf1bbc30e82069b64ede106387
Steve Hollasch <steve@hollasch.net>
2017-06-08 16:10:38 -0700

    Add admin.cmd - execute command as admin

---
+++ admin.cmd
@@ -0,0 +1,21 @@
+@setlocal & echo off
+@REM // ============================================================================================
+@REM // admin.cmd -- Administrator launch script
+@REM // 
+@REM //     This script checks the current permission level of the invoking shell. If it currently
+@REM //     has administrator privilege, it executes the command given on the remainder of the
+@REM //     command line. If the current shell does not have administrator privilege, it launches
+@REM //     a new administrator shell, and then executes the command.
+@REM // 
+@REM // ============================================================================================
+
+REM // Check for admin rights
+net file 1>nul 2>&1
+
+if %errorlevel% equ 0 (
+    %*
+) else (
+    echo Launching Administrator shell.
+    powershell -ex unrestricted ^
+        -Command "Start-Process -Verb RunAs -FilePath '%comspec%' -ArgumentList '/k %*'"
+)
