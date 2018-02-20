ef59e2c71f5b07a532a9b6218e8c14d7a09ceb16
Steve Hollasch <steve@hollasch.net>
2018-02-20 14:16:56 -0800

    clip-edit now starts with current clipboard text

--- clip-edit.cmd
+++ clip-edit.cmd
@@ -1,4 +1,8 @@
+@if (@CodeSection == @Batch) @then
 @echo off & setlocal
+
+REM // The first line skips over the shell script lines until the end statement at the bottom.
+REM // A simple trick so that this can also contain its own JScript for clipboard pasting.
 
 echo Invoking editor for clipboard.
 
@@ -6,7 +10,14 @@ if not defined EDITOR set EDITOR=notepad.exe
 
 call get-tempFile tempFileName
 
-copy nul %tempFileName% >nul 
+REM // The following line pastes the current clipboard contents into the temporary file for editing.
+REM // Unfortunately, Windows comes with a standard tool to clip content, but lacks a standard tool
+REM // to paste the (text) contents of the clipboard.
+REM //
+REM // See https://stackoverflow.com/a/15747067/566185
+
+cscript /nologo /e:JScript "%~f0" >%tempFileName%
+
 call "%EDITOR%" %tempFileName% || (
     echo get-edit.cmd: Unable to invoke "%EDITOR%". Check EDITOR environment variable. 1>&2
     exit /b 1
@@ -14,3 +25,7 @@ call "%EDITOR%" %tempFileName% || (
 
 clip < %tempFileName%
 del %tempFileName%
+goto :eof
+
+@end  // Begin JScript hybrid script
+WSH.Echo(WSH.CreateObject('htmlfile').parentWindow.clipboardData.getData('text'));
