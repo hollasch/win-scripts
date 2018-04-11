281ce2db1aeb442532ed485176f0b2a2ca7334e1
Steve Hollasch <steve@hollasch.net>
2018-04-11 12:46:03 -0700

    envgui runs as admin to allow sys variable editing

--- envgui.cmd
+++ envgui.cmd
@@ -1,1 +1,15 @@
-@start rundll32 sysdm.cpl,EditEnvironmentVariables
+@setlocal & echo off
+:: This command directly launches the Windows environment variable GUI. It's merely a timesaving way
+:: to avoid navigating through many different GUIs to change system environment variables.
+
+set args=sysdm.cpl,EditEnvironmentVariables
+
+:: Check for admin rights and run as admin if necessary.
+net file 1>nul 2>&1
+
+if %ErrorLevel% equ 0 (
+    rundll32.exe %args%
+) else (
+    powershell -ex unrestricted ^
+        -Command "Start-Process -Verb RunAs -FilePath 'rundll32.exe' -ArgumentList '%args%'"
+)
