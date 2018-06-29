7c329de49c9ad5c503569b15b4c64f176fccf557
Steve Hollasch <steve@hollasch.net>
2018-06-29 12:01:02 -0700

    Update self-admin-launch.cmd

--- snippets/self-admin-launch.cmd
+++ snippets/self-admin-launch.cmd
@@ -1,6 +1,10 @@
 @setlocal & echo off
 
-REM -- Check for admin rights and relaunch as admin if necessary.
+REM -- Check for admin rights and self-relaunch as admin if necessary.
+REM --
+REM -- Note: this script does not work if it's invoked on a network-mapped drive. To do that, you
+REM -- would need to get the full network path (parsing the output of `net map` perhaps?) and then
+REM -- construct the fully-qualified name of the script file.
 
 net file 1>nul 2>&1 && goto :admin
     echo Re-launching as Admin...
@@ -9,4 +13,6 @@ net file 1>nul 2>&1 && goto :admin
 :admin
 
 echo Running as admin.
+echo Arguments: %*
+
 net file
