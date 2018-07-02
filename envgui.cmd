64514d72f4cb5e278c152e48257d038041223660
Steve Hollasch <steve@hollasch.net>
2018-07-02 10:56:56 -0700

    Run envgui command in background

--- envgui.cmd
+++ envgui.cmd
@@ -8,7 +8,7 @@ set args=sysdm.cpl,EditEnvironmentVariables
 net file 1>nul 2>&1
 
 if %ErrorLevel% equ 0 (
-    rundll32.exe %args%
+    start rundll32.exe %args%
 ) else (
     powershell -ex unrestricted ^
         -Command "Start-Process -Verb RunAs -FilePath 'rundll32.exe' -ArgumentList '%args%'"
